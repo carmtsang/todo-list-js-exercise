2fb61da9b547d445c45b013113c29e618538d76e
Carmen Tsang <carmtsang89@hotmail.com>
2022-04-10 18:42:28 +0000

    turned the task functions into methods in the object

--- index.js
+++ index.js
@@ -1,28 +1,30 @@
-// Arrays to keep track of each task's state
-// const taskTitles = [];
-// const taskComplete = [];
-// const taskDescriptions = [];
-
-// Create a new task by adding to the arrays
 // A new task will be created as incomplete
 function newTask(title, description) {
   const task = {
     title: title,
     description: description,
-    compelte: false
+    compelte: false,
+
+    logState: function() {
+      console.log(`${this.title} has${this.complete ? " " : " not "}been completed`);
+    },
+
+    markCompleted: function() {
+      this.complete = true;
+    },
   };
   return task;
-}
+};
 
 //prints out the provided task's details
-function logTaskState(task) {
-  console.log(`${task.title} has${task.complete ? " " : " not "}been completed`);
-}
+// function logTaskState(task) {
+//   console.log(`${task.title} has${task.complete ? " " : " not "}been completed`);
+// }
 
-// Mark a task as complete by setting the task's status in the `taskComplete` array to `true`
-function completeTask(task) {
-  task.complete = true;
-}
+// // Mark a task as complete by setting the task's status in the `taskComplete` array to `true`
+// function completeTask(task) {
+//   task.complete = true;
+// };
 
 
 // DRIVER CODE BELOW
@@ -31,8 +33,12 @@ const task1 = newTask("Clean Cat Litter", "take all the 💩 out of the litter b
 const task2 = newTask('Do Laundry', '😨')
 const tasks = [task1, task2];
 
-logTaskState(task1); //Clean cat litter has not been completed
-completeTask(task1);
-logTaskState(task1); //clean cat litter has been completed
+// logTaskState(task1); //Clean cat litter has not been completed
+// completeTask(task1);
+// logTaskState(task1); //clean cat litter has been completed
+
+task1.logState(); //Clean cat litter has not been completed
+task1.markCompleted();
+task1.logState(); //clean cat litter has been completed
 
 console.log(tasks);
